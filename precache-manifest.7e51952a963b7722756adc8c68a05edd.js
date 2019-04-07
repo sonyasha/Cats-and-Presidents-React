e95da0fcb0210f6216d44cfab2322901e914f3aa
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2019-04-07 17:18:27 -0400

    Updates

---
+++ precache-manifest.7e51952a963b7722756adc8c68a05edd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "da76ff0a6070e7656a4456009ddca009",
+    "url": "/Cats-and-Presidents-React/static/media/greyfloral_@2X.da76ff0a.png"
+  },
+  {
+    "revision": "52b508cda49d3bf702e6867cc71a1e2d",
+    "url": "/Cats-and-Presidents-React/static/media/fabric_of_squares_gray_@2X.52b508cd.png"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Cats-and-Presidents-React/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ee946d9b810d90744abe",
+    "url": "/Cats-and-Presidents-React/static/js/runtime~main.124f03da.js"
+  },
+  {
+    "revision": "497db063883cb30b224a",
+    "url": "/Cats-and-Presidents-React/static/js/main.701c689e.chunk.js"
+  },
+  {
+    "revision": "b8867ed668eda365940b",
+    "url": "/Cats-and-Presidents-React/static/js/2.d51825fa.chunk.js"
+  },
+  {
+    "revision": "497db063883cb30b224a",
+    "url": "/Cats-and-Presidents-React/static/css/main.9f441e92.chunk.css"
+  },
+  {
+    "revision": "b8867ed668eda365940b",
+    "url": "/Cats-and-Presidents-React/static/css/2.7abebd52.chunk.css"
+  },
+  {
+    "revision": "af2785b2397d7f4ab689e49dccc70153",
+    "url": "/Cats-and-Presidents-React/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Cats-and-Presidents-React/precache-manifest.7e51952a963b7722756adc8c68a05edd.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Cats-and-Presidents-React/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
